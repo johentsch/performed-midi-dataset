b11ccb59ebfa4e0d067abc4ff9b569900ea04884
Francesco <foscarin.francesco@gmail.com>
2020-02-21 18:39:34 +0900

    new bach with alignments. Ready for annotations

--- util/nak_align.py
+++ util/nak_align.py
@@ -4,7 +4,8 @@ import subprocess
 
 MAESTRO_BPATH = Path("../datasets/maestro-v2.0.0/")  #you need to download the maestro dataset from https://magenta.tensorflow.org/datasets/maestro
 VIRTUOSO_BPATH = Path("./")
-NAK_PATH = Path("../nak_alignment")
+NAK_PATH = Path("./util/nak_alignment")
+NAK2DS_PATH = Path("../../")
 
 df = pd.read_pickle("performance_dataframe_v2(bach).pkl")
 align_perf = df[df["maestro_path"].notna()]
@@ -17,7 +18,22 @@ def delete_old_files(row):
     Path(VIRTUOSO_BPATH,row["performed_midi_path"][:-5]+"_infer_spr.txt").unlink()
 
 def generate_nak_align(row):
-    subprocess.call(['python3', Path(NAK_PATH,"MIDIToMIDIAlign.sh"), row["score_midi_path"], row["performed_midi_path"]])
+    subprocess.call([Path(NAK_PATH,"MIDIToMIDIAlign.sh"), Path(VIRTUOSO_BPATH,row["score_midi_path"][:-4]), Path(row["performed_midi_path"][:-4])])
 
-align_perf.apply(generate_nak_align)
-align_perf
+def update_corresp_path(row):
+    if pd.isnull(row["maestro_path"]):
+        return row["midi2midi_alignment_path"]
+    else:
+        return row["midi2midi_alignment_path"].replace("_infer_corresp.txt","M_corresp.txt")
+
+
+
+print("Creating ..................")
+align_perf.apply(generate_nak_align,axis = 1)
+print("Deleting old files ......................")
+align_perf.apply(delete_old_files,axis = 1)
+print("Updating the corresp ................")
+df["midi2midi_alignment_path"] =df.apply(update_corresp_path,axis = 1) 
+
+#save the new dataframe
+df.to_pickle("performance_dataframe_v2(bach).pkl")
